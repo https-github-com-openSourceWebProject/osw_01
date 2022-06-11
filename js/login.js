a2146e1d634e16197681c29cf307cfef3e685fe2
minwg1021 <minwg1021@gmail.com>
2022-06-11 15:34:53 +0900

    로그인 & 회원가입 부분 변경

--- js/login.js
+++ js/login.js
@@ -8,8 +8,6 @@ firebase.auth().onAuthStateChanged((user) => {
 if (localStorage.getItem("flag") == "success") {
   $("#btn-login").css("display", "none");
   $("#btn-logout").css("display", "inline");
-  // var user = localStorage.getItem("userName");
-  // $("#userName").html(JSON.parse(user).displayName);
 }
 
 $("#login").click(function () {
