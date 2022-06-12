0475cc4782871df4a62bb1d360311eea574ba84e
minwg1021 <minwg1021@gmail.com>
2022-06-12 11:12:18 +0900

    product 페이지 수정

--- js/product.js
+++ js/product.js
@@ -4,24 +4,25 @@ db.collection("product")
   .get()
   .then((result) => {
     result.forEach((doc) => {
-      console.log(doc.data());
-      //디브를 동적으로 만들어서 출력할수 있음!!
-      var template = `<div class="product"><div class="thumbnail" style="background-image: url('${
-        doc.data().이미지
-      }')"></div>
-      <div class="flex-grow-1 p-4">
-        <h5 class="title"><a href="/detail.html?id=${doc.id}"> ${
+      var template = `
+      <div class="product">
+        <div class="thumbnail" style="background-image: url('${
+          doc.data().이미지
+        }')">
+        </div>
+          <div class="flex-grow-1 p-4">
+            <h5 class="title"><a href="/detail.html?id=${doc.id}"> ${
         doc.data().제목
       } </a></h5>
-        <p class="date">${doc.data().날짜}</p>
-        <p class="price">${
-          doc
-            .data()
-            .가격.toString()
-            .replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "원"
-        }</p>
-      </div>
-      </div>`; // html을 만들어서
-      $(".item_list").append(template); // 해당 클래스에 넣어줄것
+            <p class="date">${doc.data().날짜}</p>
+            <p class="price">${
+              doc
+                .data()
+                .가격.toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "원"
+            }</p>
+          </div>
+      </div>`;
+      $("#item_list").append(template);
     });
   });
