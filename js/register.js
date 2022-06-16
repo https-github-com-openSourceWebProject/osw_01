d46bac96c48612dd4d5a24ebcb59d509eb64dde5
minwg1021 <minwg1021@gmail.com>
2022-06-16 14:17:20 +0900

    register DB 변경

--- js/register.js
+++ js/register.js
@@ -1,3 +1,5 @@
+var dbReg = firebase.firestore();
+
 $("#register").click(() => {
   var name = $("#name-new").val();
   var email = $("#email-new").val();
@@ -8,13 +10,13 @@ $("#register").click(() => {
     .createUserWithEmailAndPassword(email, password)
     .then((result) => {
       var 유저정보 = {
-        name1: name,
-        email1: email,
+        name: name,
+        email: email,
+        password: password,
       };
+      dbReg.collection("user").doc().set(유저정보);
 
-      db.collection("user").doc(result.user.uid).set(유저정보);
-
-      result.user.updateProfile({ displayName: name });
+      // result.user.updateProfile({ displayName: name });
       alert("회원가입이 완료되었습니다.");
       window.location.href = "./main.html";
     });
